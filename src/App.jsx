82427d07a1eb8481a7407689396a61ccdff922af
unknown <towfiqtomal88@gmail.com>
2025-07-01 12:56:41 +0600

    navbar done

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// run rafce to quickly create componnents.
+import React from 'react' 
+import Navbar from './sections/Navbar'
+
+const App = () => {
+  return (
+    <div className='container mx-auto max-w-7xl'>
+      {/* litreally just a div with a class name of container, mx-auto=horizontally centers whatever the div, and max-w-7xl=size(width) of the div */}
+    <Navbar />
+    {/*hero*/}
+    {/*about*/}
+    {/*projects*/}
+    {/*experience*/}
+    {/*testimonial*/}
+    {/*contact*/}
+    {/*footer*/}
+    
+    </div>
+  )
+}
+
+export default App
